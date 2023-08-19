260a9241ff4187b6a2d2d7c246fb21eb61d1557b
dev-sps <142286391+devsps1@users.noreply.github.com>
2023-08-19 13:15:53 -0400

    used timeit instead of time

--- courseProject.py
+++ courseProject.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.metrics import confusion_matrix, classification_report
-import time
+import timeit
 
 def PrintConfusionMatrix(y_test, y_pred):
     conf_matrix = confusion_matrix(y_test, y_pred)
@@ -26,6 +26,9 @@ if __name__ == "__main__":
   # Split the data into training and testing sets
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
+  print(f'Train: {X_train.shape}')
+  print(f'Test: {X_test.shape}')
+
   scaler = StandardScaler()
   X_train_scaled = scaler.fit_transform(X_train)
   X_test_scaled = scaler.transform(X_test)
@@ -36,33 +39,37 @@ if __name__ == "__main__":
   #--------------------------------------- Fisher Linear Discriminant---------------------------------------
   # training time
   print("\n\t\tFisher's Linear Discriminant\n")
-  start_time = time.time()
+  start_time = timeit.default_timer()
   clf.fit(X_train_scaled, y_train)
-  training_time = time.time() - start_time
-  print(f"Training Time: {training_time} seconds")
+  finish_time = timeit.default_timer()
+  training_time = finish_time - start_time
+  print(f"Training Time: {training_time:.5f} seconds")
 
   # testing time
-  start_time = time.time()
+  start_time = timeit.default_timer()
   y_pred = clf.predict(X_test_scaled)
-  testing_time = time.time() - start_time
-  print(f"Testing Time: {testing_time} seconds")
+  finish_time = timeit.default_timer()
+  testing_time = finish_time - start_time
+  print(f"Testing Time: {testing_time:.5f} seconds")
 
   PrintConfusionMatrix(y_test, y_pred)
   PrintClassificationReport(y_test, y_pred)
 
-  #--------------------------------------- Linear support vector machine------------------------------------
+  # --------------------------------------- Linear support vector machine------------------------------------
   # training time
   print("\n\n\t\tSupport Vector Machine\n")
-  start_time = time.time()
+  start_time = timeit.default_timer()
   svm_classifier.fit(X_train_scaled, y_train)
-  training_time = time.time() - start_time
-  print(f"Training Time: {training_time} seconds")
+  finish_time = timeit.default_timer()
+  training_time = finish_time - start_time
+  print(f"Training Time: {training_time:.5f} seconds")
 
   # testing time
-  start_time = time.time()
+  start_time = timeit.default_timer()
   y_pred = svm_classifier.predict(X_test_scaled)
-  testing_time = time.time() - start_time
-  print(f"Testing Time: {testing_time} seconds")
+  finish_time = timeit.default_timer()
+  testing_time = finish_time - start_time
+  print(f"Testing Time: {testing_time:.5f} seconds")
 
   PrintConfusionMatrix(y_test, y_pred)
   PrintClassificationReport(y_test, y_pred)
---
+++ test.py
@@ -0,0 +1,6 @@
+import time
+
+time1 = time.time()
+t = 1
+time2 = time.time()
+print(time2 - time1)
